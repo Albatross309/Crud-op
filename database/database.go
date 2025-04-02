62a28caba4bdc65709ba455bf298cb6b44a3c067
Albatross309 <krutisabalpara@gmail.com>
2025-04-02 17:38:11 +0530

    new modified

--- database/database.go
+++ database/database.go
@@ -8,19 +8,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// func DbConnection() (*sql.DB, error) {
-// 	connectionString := "root:albatross@/devbook?charset=utf8&parseTime=True&loc=Local"
-
-// 	db, err := sql.Open("mysql", connectionString)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if err = db.Ping(); err != nil {
-// 		return nil, err
-// 	}
-// 	return db, nil
-// }
-
 var DB *sql.DB
 
 func ConnectDB() {
